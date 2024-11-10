c163d1007c69c2db2e6b4d88d6e6577df20ea05d
Annushkkka <anyuta260703@gmail.com>
2024-11-10 23:06:55 +0300

    Заполнила класс клиентов

--- src/main/java/org/example/programm/Client.java
+++ src/main/java/org/example/programm/Client.java
@@ -1,4 +1,37 @@
 package org.example.programm;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "client")
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "first_name", nullable = false, length = 50)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 50)
+    private String lastName;
+
+    @Column(name = "middle_name", length = 50)
+    private String middleName;
+
+    @Column(name = "birth_date")
+    private java.sql.Date birthDate;
+
+    @Column(name = "phone", length = 15)
+    private String phone;
+
+    @Column(name = "email", length = 100)
+    private String email;
 }
