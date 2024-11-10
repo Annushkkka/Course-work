9bed83cf703dc4cff7f9cbe6907d4388b4fd4b48
Annushkkka <anyuta260703@gmail.com>
2024-11-10 23:09:10 +0300

    Заполнила класс записи

--- src/main/java/org/example/programm/Appointment.java
+++ src/main/java/org/example/programm/Appointment.java
@@ -1,4 +1,40 @@
 package org.example.programm;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "appointment")
 public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id", nullable = false)
+    private Client client;
+
+    @ManyToOne
+    @JoinColumn(name = "master_id", nullable = false)
+    private Master master;
+
+    @ManyToOne
+    @JoinColumn(name = "service_id", nullable = false)
+    private Service service;
+
+    @Column(name = "appointment_date", nullable = false)
+    private java.sql.Timestamp appointmentDate;
+
+    @Column(name = "end_time", nullable = false)
+    private java.sql.Timestamp endTime;
 }
+
