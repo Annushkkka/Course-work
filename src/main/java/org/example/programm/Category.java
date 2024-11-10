eb59e852612afd841196b98cec7c608c308b92ee
Annushkkka <anyuta260703@gmail.com>
2024-11-10 23:05:26 +0300

    Изменила класс грейдов на класс категория

---
+++ src/main/java/org/example/programm/Category.java
@@ -0,0 +1,25 @@
+package org.example.programm;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "price_multiplier", nullable = false, precision = 3, scale = 2)
+    private Double priceMultiplier;
+}
