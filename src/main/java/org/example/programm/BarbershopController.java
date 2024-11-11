67f4b6a051a2be12e5827cbed912600956aaaac9
Annushkkka <anyuta260703@gmail.com>
2024-11-12 00:38:41 +0300

    Исправлен класс мастер_услуга, добавлен файл html и частично реализован контроллер. Сейчас выводится поисковая строка и 2 кнопки: поиск и очистить.

--- src/main/java/org/example/programm/BarbershopController.java
+++ src/main/java/org/example/programm/BarbershopController.java
@@ -1,4 +1,22 @@
 package org.example.programm;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
+
+@Controller
 public class BarbershopController {
+    @Autowired
+    private BarbershopService barbershopService;
+
+    @GetMapping("/")
+    public String index(Model model, @Param("keyword")String keyword) {
+        List<Master> masterlist = barbershopService.getAllMasters(keyword);
+        model.addAttribute("masterList", masterlist);
+        model.addAttribute("keyword", keyword);
+        return "index";
+    }
 }
--- src/main/java/org/example/programm/MasterService.java
+++ src/main/java/org/example/programm/MasterService.java
@@ -2,15 +2,16 @@ package org.example.programm;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import jakarta.persistence.Column;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.IdClass;
 import lombok.Data;
 
 @Entity
 @Data
 @Table(name = "master_service")
+@IdClass(MasterService.class)
 public class MasterService {
 
     @Id
