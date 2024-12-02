23acfbbb3e3e33d517c0ef405c907a02d7768834
Annushkkka <anyuta260703@gmail.com>
2024-12-03 00:22:29 +0300

    добавила в контроллер обработку ошибок

--- src/main/java/org/example/programm/BarbershopController.java
+++ src/main/java/org/example/programm/BarbershopController.java
@@ -16,155 +16,242 @@ public class BarbershopController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("message", "Добро пожаловать в барбершоп!");
-        return "index";
+        try {
+            model.addAttribute("message", "Добро пожаловать в барбершоп!");
+            return "index";
+        } catch (Exception e) {
+            return "error";
+        }
     }
     @GetMapping("/master/")
-    public String master(Model model, @Param("keyword")String keyword) {
-        List<Master> masterlist = barbershopService.getAllMasters(keyword);
-        model.addAttribute("masterList", masterlist);
-        model.addAttribute("keyword", keyword);
-        return "master";
+    public String master(Model model, @Param("keyword") String keyword) {
+        try {
+            List<Master> masterlist = barbershopService.getAllMasters(keyword);
+            model.addAttribute("masterList", masterlist);
+            model.addAttribute("keyword", keyword);
+            return "master";
+        } catch (Exception e) {
+            return "error";
+        }
     }
 
     @GetMapping("/master/delete/{id}")
     public String deletemaster(@PathVariable Integer id) {
-        barbershopService.deleteMaster(id);
-        return "redirect:/master/";
+        try {
+            barbershopService.deleteMaster(id);
+            return "redirect:/master/";
+        } catch (Exception e) {
+            return "error";
+        }
     }
 
     @RequestMapping("/master/new")
     public String newmaster(Model model) {
-        Master master = new Master();
-        model.addAttribute("master", new Master());
-        return "newmaster";
+        try {
+            Master master = new Master();
+            model.addAttribute("master", new Master());
+            return "newmaster";
+        } catch (Exception e) {
+            return "error";
+        }
     }
 
     @PostMapping("/master/save")
     public String save(@ModelAttribute("master") Master master) {
-        barbershopService.saveMaster(master);
-        return "redirect:/master/";
+        try {
+            barbershopService.saveMaster(master);
+            return "redirect:/master/";
+        } catch (Exception e) {
+            return "error";
+        }
     }
 
     @GetMapping("/master/edit/{id}")
     public ModelAndView editMaster(@PathVariable Integer id) {
-        ModelAndView mav = new ModelAndView("editmaster");
-        Master master = barbershopService.getMaster(id);
-        mav.addObject("master", master);
-        return mav;
+        try {
+            ModelAndView mav = new ModelAndView("editmaster");
+            Master master = barbershopService.getMaster(id);
+            mav.addObject("master", master);
+            return mav;
+        } catch (Exception e) {
+            return new ModelAndView("error");
+        }
     }
 
     @GetMapping("/service/")
-    public String service(Model model, @Param("keyword")String keyword) {
-        List<Service> servicelist = barbershopService.getAllServices(keyword);
-        model.addAttribute("serviceList", servicelist);
-        model.addAttribute("keyword", keyword);
-        return "service";
+    public String service(Model model, @Param("keyword") String keyword) {
+        try {
+            List<Service> servicelist = barbershopService.getAllServices(keyword);
+            model.addAttribute("serviceList", servicelist);
+            model.addAttribute("keyword", keyword);
+            return "service";
+        } catch (Exception e) {
+            return "error";
+        }
     }
 
     @GetMapping("/service/delete/{id}")
     public String deleteService(@PathVariable Integer id) {
-        barbershopService.deleteService(id);
-        return "redirect:/service/";
+        try {
+            barbershopService.deleteService(id);
+            return "redirect:/service/";
+        } catch (Exception e) {
+            return "error";
+        }
     }
 
     @RequestMapping("/service/new")
     public String newService(Model model) {
-        Service service = new Service();
-        model.addAttribute("service", service);
-        return "newservice"; // Шаблон для создания новой услуги
+        try {
+            Service service = new Service();
+            model.addAttribute("service", service);
+            return "newservice"; // Шаблон для создания новой услуги
+        } catch (Exception e) {
+            return "error";
+        }
     }
 
     @PostMapping("/service/save")
     public String saveService(@ModelAttribute("service") Service service) {
-        barbershopService.saveService(service);
-        return "redirect:/service/";
+        try {
+            barbershopService.saveService(service);
+            return "redirect:/service/";
+        } catch (Exception e) {
+            return "error";
+        }
     }
 
     @GetMapping("/service/edit/{id}")
     public ModelAndView editService(@PathVariable Integer id) {
-        ModelAndView mav = new ModelAndView("editservice");
-        Service service = barbershopService.getService(id);
-        mav.addObject("service", service);
-        return mav;
+        try {
+            ModelAndView mav = new ModelAndView("editservice");
+            Service service = barbershopService.getService(id);
+            mav.addObject("service", service);
+            return mav;
+        } catch (Exception e) {
+            return new ModelAndView("error");
+        }
     }
 
     @GetMapping("/client/")
-    public String client(Model model, @Param("keyword")String keyword) {
-        List<Client> clientlist = barbershopService.getAllClient(keyword);
-        model.addAttribute("clientList", clientlist);
-        model.addAttribute("keyword", keyword);
-        return "client";
+    public String client(Model model, @Param("keyword") String keyword) {
+        try {
+            List<Client> clientlist = barbershopService.getAllClient(keyword);
+            model.addAttribute("clientList", clientlist);
+            model.addAttribute("keyword", keyword);
+            return "client";
+        } catch (Exception e) {
+            return "error";
+        }
     }
 
     @GetMapping("/client/delete/{id}")
     public String deleteClient(@PathVariable Integer id) {
-        barbershopService.deleteClient(id);
-        return "redirect:/client/";
+        try {
+            barbershopService.deleteClient(id);
+            return "redirect:/client/";
+        } catch (Exception e) {
+            return "error";
+        }
     }
 
     @RequestMapping("/client/new")
     public String newClient(Model model) {
-        Client client = new Client();
-        model.addAttribute("client", client);
-        return "newclient"; // Шаблон для создания нового клиента
+        try {
+            Client client = new Client();
+            model.addAttribute("client", client);
+            return "newclient"; // Шаблон для создания нового клиента
+        } catch (Exception e) {
+            return "error";
+        }
     }
 
     @PostMapping("/client/save")
     public String saveClient(@ModelAttribute("client") Client client) {
-        barbershopService.saveClient(client);
-        return "redirect:/client/";
+        try {
+            barbershopService.saveClient(client);
+            return "redirect:/client/";
+        } catch (Exception e) {
+            return "error";
+        }
     }
 
     @GetMapping("/client/edit/{id}")
     public ModelAndView editClient(@PathVariable Integer id) {
-        ModelAndView mav = new ModelAndView("editclient");
-        Client client = barbershopService.getClient(id);
-        mav.addObject("client", client);
-        return mav;
+        try {
+            ModelAndView mav = new ModelAndView("editclient");
+            Client client = barbershopService.getClient(id);
+            mav.addObject("client", client);
+            return mav;
+        } catch (Exception e) {
+            return new ModelAndView("error");
+        }
     }
 
     @GetMapping("/master_service/")
-    public String masterservice(Model model, @Param("keyword")String keyword) {
-        List<MasterService> masterServiceList = barbershopService.getAllMasterService(keyword);
-        model.addAttribute("masterServiceList", masterServiceList);
-        model.addAttribute("keyword", keyword);
-        return "master_service";
+    public String masterservice(Model model, @Param("keyword") String keyword) {
+        try {
+            List<MasterService> masterServiceList = barbershopService.getAllMasterService(keyword);
+            model.addAttribute("masterServiceList", masterServiceList);
+            model.addAttribute("keyword", keyword);
+            return "master_service";
+        } catch (Exception e) {
+            return "error";
+        }
     }
 
     @GetMapping("/master_service/delete/{id}")
     public String deleteMasterService(@PathVariable Integer id) {
-        barbershopService.deleteMasterService(id);
-        return "redirect:/master_service/";
+        try {
+            barbershopService.deleteMasterService(id);
+            return "redirect:/master_service/";
+        } catch (Exception e) {
+            return "error";
+        }
     }
 
     @RequestMapping("/master_service/new")
     public String newMasterService(Model model) {
-        MasterService MasterService = new MasterService();
-        model.addAttribute("MasterService", MasterService);
-        return "newmaster_service"; // Шаблон для создания нового клиента
+        try {
+            MasterService MasterService = new MasterService();
+            model.addAttribute("MasterService", MasterService);
+            return "newmaster_service"; // Шаблон для создания нового клиента
+        } catch (Exception e) {
+            return "error";
+        }
     }
 
     @PostMapping("/master_service/save")
     public String saveMasterService(@ModelAttribute("MasterService") MasterService MasterService) {
-        barbershopService.saveMasterService(MasterService);
-        return "redirect:/master_service/";
+        try {
+            barbershopService.saveMasterService(MasterService);
+            return "redirect:/master_service/";
+        } catch (Exception e) {
+            return "error";
+        }
     }
 
     @GetMapping("/master_service/edit/{id}")
     public ModelAndView editMasterService(@PathVariable Integer id) {
-        ModelAndView mav = new ModelAndView("editMasterService");
-        MasterService MasterService = barbershopService.getMasterService(id);
-        mav.addObject("MasterService", MasterService);
-        return mav;
+        try {
+            ModelAndView mav = new ModelAndView("editMasterService");
+            MasterService MasterService = barbershopService.getMasterService(id);
+            mav.addObject("MasterService", MasterService);
+            return mav;
+        } catch (Exception e) {
+            return new ModelAndView("error");
+        }
     }
 
-
     @GetMapping("/category/")
-    public String category(Model model, @Param("keyword")String keyword) {
-        List<Category> categoryList = barbershopService.getAllCategory(keyword);
-        model.addAttribute("categoryList", categoryList);
-        model.addAttribute("keyword", keyword);
-        return "category";
+    public String category(Model model, @Param("keyword") String keyword) {
+        try {
+            List<Category> categoryList = barbershopService.getAllCategory(keyword);
+            model.addAttribute("categoryList", categoryList);
+            model.addAttribute("keyword", keyword);
+            return "category";
+        } catch (Exception e) {
+            return "error";
+        }
     }
 }
